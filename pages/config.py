ba3306e61833ccb77bc66af393863fc18ad5c101
ChrisTheB97 <kikokunzel@gmail.com>
2025-10-01 14:39:51 +0200

    Create config.py

---
+++ pages/config.py
@@ -0,0 +1,6 @@
+# config.py — vyplň si podľa seba (použije sa v appke aj v PDF)
+STUDENT_NAME = "Tvoje meno a priezvisko"
+STUDENT_CONTACT = "tvoj.email@example.com"
+SCHOOL = "Názov školy / fakulty"
+COURSE = "informatika_ZS2025_14–17"
+DEFAULT_UNITS = "m"  # predvolené jednotky osí
